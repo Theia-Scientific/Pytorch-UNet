c8f62250eda184e5679c11cc308d6510fa6c1eb1
Christopher Field <chris.field@theiascientific.com>
2022-10-30 14:39:27 -0400

    Remove leading prefix from entrypoint
    The "unet_" is removed because it is already known that this model is a
    UNet model by virtue of GitHub repository URL and the usage of this
    codebase.

--- hubconf.py
+++ hubconf.py
@@ -2,7 +2,7 @@ import torch
 from unet import UNet as _UNet
 
 
-def unet_carvana(pretrained=False, scale=0.5):
+def carvana(pretrained=False, scale=0.5):
     """
     UNet model trained on the Carvana dataset ( https://www.kaggle.com/c/carvana-image-masking-challenge/data ).
     Set the scale to 0.5 (50%) when predicting.
